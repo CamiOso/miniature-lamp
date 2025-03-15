84a6b94703c07b3f7b9edf588163d8c3f57ae3a2
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2025-03-15 10:56:08 -0500

    Se crea el log para el manejo de las transacciones

--- src/bank_account.py
+++ src/bank_account.py
@@ -1,16 +1,26 @@
 class BankAccount:
-    def __init__(self,balance=0):
+    def __init__(self,balance=0,log_file=None):
         self.balance=balance
+        self.log_file=log_file
+        self._log_transaction('Cuenta Creada')
+
+    def _log_transaction(self,message):
+        if self.log_file:
+            with open(self.log_file,'a') as f:
+                f.write(f"{message }\n")
 
     def deposit(self,amount):
         if amount>0:
             self.balance+=amount
+            self._log_transaction(f"Deposito {amount}. Nuevo Balance :{self.balance}")
         return self.balance
     
     def withdraw(self,amount):
         if amount>0:
             self.balance-=amount
+            self._log_transaction(f"Retiro {amount}. Nuevo Balance :{self.balance}")
         return self.balance
     
     def get_balance(self):
+        self._log_transaction(f"Consulta de Balance :{self.balance}")
         return self.balance
